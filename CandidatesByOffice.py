135b2f3bfe975f7119ca0a05796162f8bafceb7b
kre64 <kre9404@gmail.com>
2018-11-10 18:41:46 -0500

    minor formatting changes, added CORS for React development

--- CandidatesByOffice.py
+++ CandidatesByOffice.py
@@ -1,6 +1,8 @@
-#Going to 127.0.0.1:5000/getCandidatesByOffice will return json object with offices that are up
-#for election with the candidates that are running for those offices along with to what party
-#they belong
+'''
+Going to 127.0.0.1:5000/getCandidatesByOffice will return json object with
+offices that are up for election with the candidates that are running for
+those offices along with to what party they belong
+'''
 from flask import Flask, jsonify, json
 from apiclient.discovery import build
 
@@ -8,26 +10,51 @@ API_KEY = open("api.txt", "r")
 service = build('civicinfo', 'v2', developerKey=API_KEY.readline())
 app = Flask(__name__)
 
-address = "Brooklyn 3039 Ocean Pkwy NY 11235"
-request = service.elections().voterInfoQuery(address=address, returnAllAvailableData=True)
-response = request.execute()
+'''
+encapsulate this request to GoogleCivic so it's called from within our endpoint
 
-@app.route('/getCandidatesByOffice')
+address = "Brooklyn 3039 Ocean Pkwy NY 11235"
+request = service.elections().voterInfoQuery(
+    address=address,
+    electionId=2000,
+    returnAllAvailableData=True)
+response = request.execute()
+'''
+
+
+@app.route('/getCandidatesByOffice', methods=['GET', 'POST'])
 def getCandidatesByOffice():
+    address = "Brooklyn 3039 Ocean Pkwy NY 11235"
+    request = service.elections().voterInfoQuery(
+        address=address,
+        electionId=2000,
+        returnAllAvailableData=True
+        )
+    response = request.execute()
+
     candidate = []
     offices = []
-    for office in response.get('contests',[]):
+    for office in response.get('contests', []):
         offices.append(office.get('office'))
-    for reps in response.get('contests',[]):
+    for reps in response.get('contests', []):
         for office in offices:
-            if office == reps.get('office'):
+            if office == reps.get('office') and office is not None:
                 for individual in reps.get('candidates'):
-                    individual_info = []
-                    individual_info.append(individual.get('name'))
-                    individual_info.append(individual.get('party'))
-                    candidate.append({office : individual_info })
+                    if individual is not None:
+                        individual_info = []
+                        individual_info.append(individual.get('name'))
+                        individual_info.append(individual.get('party'))
+                        candidate.append({office: individual_info})
     jsonStr = json.dumps(candidate)
-    return jsonify (positions_and_candidates = jsonStr)
+
+    # return jsonify (positions_and_candidates = jsonStr)
+
+    # I need these because I'm working on different ports with ReactJs
+    # print(response)
+    resp = jsonify(positions_and_candidates=jsonStr)
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp
+
 
 if __name__ == '__main__':
-    app.run(port = 5000, debug = True)
+    app.run(port=5000, debug=True)

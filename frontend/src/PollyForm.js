732a00f9370c8cf9b36d47ac7087184e358bd1a8
kre64 <kre9404@gmail.com>
2018-11-21 00:00:10 -0500

    reworked props constructor, pollyForm for query by address only

--- frontend/src/PollyForm.js
+++ frontend/src/PollyForm.js
@@ -1,103 +1,80 @@
 import React from 'react';
 import $ from 'jquery'; 
-
+import Candidate from './Candidate'
 // Form used for ADDRESS and SPECIFIC CANDIDATE querying
 
 class PollyForm extends React.Component {
     constructor(props){
         super(props)
-        let pollyform = [];
 
-        this.props.data.forEach((i) => {
-            pollyform.push({
-                title: i.title,
-                content: i.content,
-                open: false
-            });
-        });
-
-        this.state = {pollyContent: pollyform};
+        this.state = {hasData: false};
     }
 
-    submit(i, title) {
-        /*
-        Handle cases for three buttons:
-        by specific name
-        by address
-        third button displays links to voting info
-        
-        SWITCH HANDLES DIFFERENT SEARCH BARS
-        Address,
-        Specific,
-        */
-        switch(title) {
-            case "Search by Address":
-                console.log("Search by Address")
-                $.ajax({
-                    url : 'http://localhost:5000/getCandidatesByOffice',
-                    type: 'GET',
-                    processData: false,
-                    contentType: false,
-                    cache: false,
-                    dataType: 'json',
-                    success:(data)=>{
-                        let newForm = this.state.pollyContent.slice();
-                        let index = newForm.indexOf(i)
-                        console.log(data)
-                        // console.log(data.positions_and_candidates[0][0][0])
-                        newForm[0].content = data.positions_and_candidates[0][0][0]
-                        newForm[index].open = !newForm[index].open;
-                        this.setState({pollyContent: newForm});
-                    },
-                    error:function(){
-                        console.log("Error.")
-                    }
-                })
-                break;
-            case "Search Specific":
-                // Needs implementing!
-                console.log("Search Specific")
-                let newForm = this.state.pollyContent.slice();
-                let index = newForm.indexOf(i)
-                newForm[index].open = !newForm[index].open;
-                this.setState({pollyContent: newForm});
-                break;
-            default:
-                console.warn("Error in title")
-        }
+    submit(i) {
+        $.ajax({
+            url : 'http://localhost:5000/getCandidatesByOffice',
+            type: 'GET',
+            processData: false,
+            contentType: false,
+            cache: false,
+            dataType: 'json',
+            success:(data)=>{
+                
+                
+                for(var i=0; i<data.positions_and_candidates.length; i++){
+                    console.log(
+                        data.positions_and_candidates[i].name,
+                        data.positions_and_candidates[i].office,
+                        data.positions_and_candidates[i].party
+                    )
+                }
+                // console.log(data.positions_and_candidates[0][0][0])
+                // this.props.content = data.positions_and_candidates[0][0][0]
+                // this.props.open = !this.props.open;
+                this.setState({hasData: true});
+            },
+            error:function(){
+                console.log("Error.")
+            }
+        })
     }
 
     render() {
-        const sections = this.state.pollyContent.map((i) => (
-            <div key={this.state.pollyContent.indexOf(i)}>
+
+
+        return (
+            <div className="pollyform">
                 <div className="title">
                     <div className="arrow-wrapper">
-                        <i className={i.open ? "fa fa-angle-down fa-rotate-180" : "fa fa-angle-down"}></i>
+                        <i className={this.props.open ? "fa fa-angle-down fa-rotate-180" : "fa fa-angle-down"}></i>
                     </div>
                     <span className="title-text">
-                        <input className="form-control" type="text" placeholder={i.title} 
+                        <input className="form-control" type="text" placeholder={this.props.title} 
                         onKeyPress={(e) => {
                             if(e.key === 'Enter'){
-                                this.submit(i, i.title)
+                                this.submit(this.i)
                             }
                         }}>
                         </input>
                     </span>
                 </div>
-                <div className={i.open ? "content content-open" : "content"}>
-                    <div className={i.open ? "content-text content-text-open" : "content-text"}>
-                        {i.content}
+                <div className={this.props.open ? "content content-open" : "content"}>
+                    <div className={this.props.open ? "content-text content-text-open" : "content-text"}>
+                        {this.renderList()}
                     </div>
                 </div>
             </div>
-        ));
+        )
+    }
 
+    renderList() {
         return (
-            <div className="pollyform">
-            {sections}
+            <div>
+                <Candidate data={this.props.data.candidates} /> a
             </div>
         )
     }
+
 }
 
 export default PollyForm;

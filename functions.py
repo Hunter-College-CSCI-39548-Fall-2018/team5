a7013cdd9ab81b479f2b7b2bd7b7058aa5d352b8
ABortoc <abortoc@gmail.com>
2018-11-21 21:23:39 -0500

    attempting to create a function that reads frontend input

--- functions.py
+++ functions.py
@@ -3,47 +3,25 @@
 #they belong
 from flask import Flask, jsonify, json, current_app
 from apiclient.discovery import build
-from votesmart import votesmart
 import csv, requests, pprint
 import xml.etree.ElementTree as ET
 
-API_KEY1 = open("google_api.txt", "r")
-service = build('civicinfo', 'v2', developerKey=API_KEY1.readline())
+GOOGLE_API_FILE = open("google_api.txt", "r")
+service = build('civicinfo', 'v2', developerKey=GOOGLE_API_FILE.readline())
 
 VOTESMART_API_FILE = open("votesmart_api.txt", "r")
 VOTESMART_API_KEY = VOTESMART_API_FILE.readline()
 
 app = Flask(__name__)
 
-address = "Brooklyn 2164 Stuart St NY 11229"
-request = service.elections().voterInfoQuery(address=address, electionId=2000, returnAllAvailableData=True)
-response = request.execute()
-requestElections = service.elections().electionQuery()
-responseElections = requestElections.execute()
-
-#@app.route('/getElections')
-#def getElections():
-    #elections = []
-    #for election in responseElections.get():
-        #elections.append
-
-
-@app.route('/getCandidatesByOffice', methods=['GET', 'POST'])
-def getCandidatesByOffice():
-    '''
-    encapsulate this request to GoogleCivic for each request made to
-    this endpoint
-
-    address = "Brooklyn 3039 Ocean Pkwy NY 11235"
-    request = service.elections().voterInfoQuery(
-        address=address,
-        electionId=2000,
-        returnAllAvailableData=True)
+#function to get candidates and offices they are running for based on an address
+def getCandidatesByOffice(address):
+    request = service.elections().voterInfoQuery(address=address, electionId=2000, returnAllAvailableData=True)
     response = request.execute()
-    '''
 
     candidate = []
     offices = []
+
     for office in response.get('contests', []):
         offices.append(office.get('office'))
     for reps in response.get('contests', []):
@@ -51,25 +29,29 @@ def getCandidatesByOffice():
             if office == reps.get('office') and office is not None:
                 for individual in reps.get('candidates'):
                     if individual is not None:
-                        individual_info = []
-                        individual_info.append(office)
-                        individual_info.append(individual.get('name'))
-                        individual_info.append(individual.get('party'))
-                        candidate.append({i: individual_info})
-    # jsonStr = json.dumps(candidate)
+                        candidate.append({
+                            'office': office,
+                            'name': individual.get('name'),
+                            'party': individual.get('party')
+                        })
 
-    # I need these because I'm working on different ports with ReactJs
-    # jsonify candidates directly to make the frontend response neater
     resp = jsonify(positions_and_candidates=candidate)
     resp.headers.add('Access-Control-Allow-Origin', '*')
+    resp.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     return resp
 
+
+@app.route('/getCandidatesByOffice', methods=['GET', 'POST'])
+def areaSearch():
+    if request.method == 'GET':
+        address = request.args.get('address', None)
+        if address:
+            candidates_by_area = getCandidatesByOffice(address)
+            return candidates_by_area
+
+
 #Currently one way to find candidate ids, more work is needed here
-candidate_id = 0
-firstName = "Andrew"
-lastName = "Cuomo"
-state = "NY"
-def getCandidateId(firstName, lastName):
+def getCandidateId(firstName, lastName, state):
     with open('id_matrix.csv', mode='r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -77,13 +59,13 @@ def getCandidateId(firstName, lastName):
             if firstName in row:
                 if lastName in row:
                     if state in row:
-                        #print(row)
                         candidate_id = row[0]  #can have multiples, need to think about it
                         line_count += 1
             line_count += 1
     return candidate_id
 
-candidate_id = getCandidateId(firstName, lastName)
+candidate_id = getCandidateId("Alexandria", "Ocasio-Cortez", "NY")
+#print candidate_id
 
 #Going to http://127.0.0.1:5000/getCandidatesInfo/id/<candidateId> will return json about the selected candidate
 @app.route('/getCandidatesInfo/id/<candidate_id>', methods=['GET', 'POST'])

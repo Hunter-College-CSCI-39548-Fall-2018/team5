d669b3cafd7948c6b6aeda99c8d5bcf18c7589d6
kre64 <kre9404@gmail.com>
2018-11-21 22:00:20 -0500

    Example react class for Kishan

--- frontend/src/CandidateProfile.js
+++ frontend/src/CandidateProfile.js
@@ -2,3 +2,22 @@ import React from 'react';
 
 // This class should take a candidate as prop from some method
 // and display the candidates profile page
+class CandidateProfile extends React.Component {
+    constructor(props){
+        super(props)
+
+        console.log(this.props)
+        console.log(this.props.cand_dat.name)
+    }
+
+    render() {
+        
+        return (
+            <div>
+                {this.props.cand_dat.name}
+            </div>
+        )
+    }
+}
+
+export default CandidateProfile;

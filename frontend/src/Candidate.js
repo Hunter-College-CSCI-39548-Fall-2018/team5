27df9f511859d27f11b55dbc447e0e571a458767
kre64 <kre9404@gmail.com>
2018-11-23 20:53:04 -0500

    candidate profile fleshed out, proper listing of candidates

--- frontend/src/Candidate.js
+++ frontend/src/Candidate.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import CandidateProfile from './CandidateProfile';
 
 // Inner element rendered for candidate list
 
@@ -7,17 +9,39 @@ class Candidate extends React.Component {
         super(props)
         let cand_info = [];
 
-        console.log(this.props.data)
+        //console.log(this.props.data)
 
         this.state = {candidateInfo: cand_info};
     }
 
     render() {
+        const candidates = this.props.data.map((i) => (
+            <div key={this.props.data.indexOf(i)}>
+                <div className="candidate">
+                    <div className="row">
+                        <div className="col">{i.office}</div>
+                        <div className="col">
+                            <Link to={"/getCandidatesInfo/" + i.name + i.party}>{i.name}</Link>
+                        </div>
+                        <div className="col">{i.party}</div>
+                    </div>
+                    <Route 
+                        path={"/getCandidatesInfo/" + i.name + i.party} 
+                        render={(props) => (
+                            <CandidateProfile {...props} opened={false} />
+                        )}
+                    />
+                    
+                </div>
+            </div>
+        ));
         
         return (
-            <div className="candidate">
-                
+            <Router>
+            <div className="candwrapper">
+                {candidates}
             </div>
+            </Router>
         )
     }
 }
--- frontend/src/CandidateProfile.js
+++ frontend/src/CandidateProfile.js
@@ -6,15 +6,28 @@ class CandidateProfile extends React.Component {
     constructor(props){
         super(props)
 
-        console.log(this.props)
-        console.log(this.props.cand_dat.name)
+        console.log(this)
+
+        this.state = {opened: this.props.opened};
     }
 
     render() {
         
         return (
-            <div>
-                {this.props.cand_dat.name}
+            <div className="candyprofilewrapper">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col text-left">
+                            top left
+                        </div>
+                        <div className="col text-center">
+                            <h1>Candidate Profile</h1>
+                        </div>
+                        <div className="col text-right">
+                            <h1>X</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

9f3efe66befac6e487d105b85f095f99559bbaf4
kre64 <kre9404@gmail.com>
2018-11-23 20:57:30 -0500

    no longer rendering the candidate profiles as examples, proper react dom is now passing props and states to children

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PollyForm from './PollyForm'
-import CandidateProfile from './CandidateProfile'
+// import CandidateProfile from './CandidateProfile'
 import Header from './Header'
 // See ../node_modules/bootstrap/scss/_variables.scss to adjust bootstrap variables
 import '../node_modules/bootstrap/scss/bootstrap.scss'
@@ -14,16 +14,10 @@ class App extends React.Component {
             title: 'Search by Address',
             content: ''
         }
-        let cand_dat = {
-            name: 'name',
-            year: 'year',
-            party: 'party'
-        }
         return (
             <div>
                 <Header title="PollyWannaVote" />
                 <PollyForm data={data} />
-                <CandidateProfile cand_dat={cand_dat} />
             </div>
         );
     }

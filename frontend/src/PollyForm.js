f7d12a1b30aaa07c45ba7ff527b4baeba2f46943
kre64 <kre9404@gmail.com>
2018-11-23 20:56:15 -0500

    passing json data to Candidate component

--- frontend/src/PollyForm.js
+++ frontend/src/PollyForm.js
@@ -9,6 +9,7 @@ class PollyForm extends React.Component {
 
         this.state = {
             has_data: false,
+            open: false,
             candidates: []
         };
     }
@@ -23,19 +24,21 @@ class PollyForm extends React.Component {
             dataType: 'json',
             success:(data)=>{
                 
-                
+                /*
                 for(var i=0; i<data.positions_and_candidates.length; i++){
                     console.log(
                         data.positions_and_candidates[i].name,
                         data.positions_and_candidates[i].office,
                         data.positions_and_candidates[i].party
                     )
-                }
+                }*/
                 // console.log(data.positions_and_candidates[0][0][0])
                 // this.props.content = data.positions_and_candidates[0][0][0]
                 // this.props.open = !this.props.open;
+                this.props.data.open = !this.props.data.open;
                 this.setState({
-                    hasData: true,
+                    has_data: true,
+                    candidates: data.positions_and_candidates
                 });
             },
             error:function(){
@@ -51,10 +54,10 @@ class PollyForm extends React.Component {
             <div className="pollyform">
                 <div className="title">
                     <div className="arrow-wrapper">
-                        <i className={this.props.open ? "fa fa-angle-down fa-rotate-180" : "fa fa-angle-down"}></i>
+                        <i className={this.props.data.open ? "fa fa-angle-down fa-rotate-180" : "fa fa-angle-down"}></i>
                     </div>
                     <span className="title-text">
-                        <input className="form-control" type="text" placeholder={this.props.title} 
+                        <input className="form-control" type="text" placeholder={this.props.data.title} 
                         onKeyPress={(e) => {
                             if(e.key === 'Enter'){
                                 this.submit(this.i)
@@ -63,9 +66,9 @@ class PollyForm extends React.Component {
                         </input>
                     </span>
                 </div>
-                <div className={this.props.open ? "content content-open" : "content"}>
-                    <div className={this.props.open ? "content-text content-text-open" : "content-text"}>
-                        {this.renderList()}
+                <div className={this.props.data.open ? "content content-open" : "content"}>
+                    <div className={this.props.data.open ? "content-text content-text-open" : "content-text"}>
+                        {this.state.has_data ? this.renderList() : "noData"}
                     </div>
                 </div>
             </div>
@@ -75,7 +78,7 @@ class PollyForm extends React.Component {
     renderList() {
         return (
             <div>
-                <Candidate data={this.state.has_data ? this.state.candidates : this.state.has_data} /> a
+                <Candidate data={this.state.has_data ? this.state.candidates : this.state.has_data} />
             </div>
         )
     }

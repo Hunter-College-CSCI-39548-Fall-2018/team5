9ab470047d5dd34111b4119356bd69acd882bcb5
kre64 <kre9404@gmail.com>
2018-12-01 18:18:06 -0500

    email skeleton setup

--- frontend/src/EmailForm.js
+++ frontend/src/EmailForm.js
@@ -5,15 +5,49 @@ class EmailForm extends React.Component {
         super(props)
 
         this.state = {
-            has_email: false
+            email: ""
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const name = event.target.name
+        this.setState({
+            [name]: event.target.value
+        });
+        console.log(name, ":", event.target.value, this.state)
+    }
+
+    handleSubmit(event) {
+        alert('submit!');
+        event.preventDefault();
+        this.submit()
+    }
+
+    submit(i) {
+        console.log("Implement Yins email route")
     }
 
     render() {
         
         return (
-            <div>
-                replace with email form
+            <div className="pollyform">
+                <div className="email">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-row">
+                        <div className="col">
+                            <input className="form-control" type="text"
+                            name="email"
+                            placeholder={"Email Address"}
+                            onChange={this.handleChange}
+                            />
+                        </div>
+                        <input type="submit" value="Submit" />
+                    </div>
+                </form>
+                </div>
             </div>
         )
     }

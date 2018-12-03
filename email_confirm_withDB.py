c8b5f3d818ec6a2b2f113956749aef3bacca4326
c135792468 <c1357902468@gmail.com>
2018-12-03 11:48:01 -0500

    Add files via upload

---
+++ email_confirm_withDB.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, url_for
+from flask_mail import Mail, Message
+from flaskext.mysql import MySQL
+import time
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+mail = Mail(app)
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return '<form action="/" method="POST"><input name="email"><input type="submit"></form>'
+    if request.method =='POST':
+        email = request.form['email']
+        con = mysql.get_db()
+        c = con.cursor()
+        c.execute('SELECT count(*) from acc where email = %s group by email',(email))
+        auth = c.rowcount
+        if(auth == 0):
+           c.execute('insert into acc(email, confirm) values(%s, false)', (email))
+           con.commit()
+           c.close()
+           msg = Message('Confirm Email', sender='test11aatest@gmail.com', recipients=[email])
+           ts = time.time()
+           link = url_for('confirm_email', ts=ts, email=email, _external=True)
+           msg.body = 'Your link is {}'.format(link)
+           mail.send(msg)
+           return '<h1>Please cofirm your e-mail{}.</h1>'.format(auth)
+        else:
+            return '<h1>e-mail exist: {}</h1>'.format(auth)
+
+           
+
+@app.route('/confirm_email/<ts>/<email>')
+def confirm_email(ts,email):
+    con = mysql.get_db()
+    c = con.cursor()
+    c.execute('UPDATE acc SET confirm = true WHERE email = %s',(email))
+    con.commit()
+    c.close()
+    return '<h1>E-mail has Confirmed: {}</h1>'.format(email)
+
+if __name__ == '__main__':
+    app.run(debug=True)

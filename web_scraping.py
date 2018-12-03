f5b0fd081853b768dda7d9ebcf6fcea530ca903d
c135792468 <c1357902468@gmail.com>
2018-12-03 09:03:02 -0500

    Add files via upload

---
+++ web_scraping.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, url_for, jsonify, Response
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+app = Flask(__name__)
+
+@app.route('/<name>', methods=['GET', 'POST'])
+def index(name):
+    url = "https://www.google.co.in/search?q=" + name;
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    topic = []
+
+    for item in soup.select(".r a"):
+        start = "/url?q="
+        end = "&sa=U&ved="
+        href = item.get('href')
+        url = href[href.find(start)+len(start):href.rfind(end)]
+        topic.append({'topic': item.text, 'url': url})
+        
+    #two return formate
+    #return Response(json.dumps(topic))
+    return jsonify(results = topic)
+
+if __name__ == '__main__':
+    app.run(debug=True)

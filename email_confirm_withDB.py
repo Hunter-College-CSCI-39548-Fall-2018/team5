d462170e463329b8533c8c6b69007c9bdf0fb879
c135792468 <c1357902468@gmail.com>
2018-12-03 12:12:30 -0500

    Update email_confirm_withDB.py

--- email_confirm_withDB.py
+++ email_confirm_withDB.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, url_for
 from flask_mail import Mail, Message
 from flaskext.mysql import MySQL
+from validate_email import validate_email
 import time
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
@@ -13,23 +14,26 @@ def index():
         return '<form action="/" method="POST"><input name="email"><input type="submit"></form>'
     if request.method =='POST':
         email = request.form['email']
-        con = mysql.get_db()
-        c = con.cursor()
-        c.execute('''Create TABLE IF NOT EXISTS acc(ID int Primary key auto_increment, email varchar(32), confirm Boolean)''')
-        c.execute('SELECT count(*) from acc where email = %s group by email',(email))
-        auth = c.rowcount
-        if(auth == 0):
-           c.execute('insert into acc(email, confirm) values(%s, false)', (email))
-           con.commit()
-           c.close()
-           msg = Message('Confirm Email', sender='test11aatest@gmail.com', recipients=[email])
-           ts = time.time()
-           link = url_for('confirm_email', ts=ts, email=email, _external=True)
-           msg.body = 'Your link is {}'.format(link)
-           mail.send(msg)
-           return '<h1>Please cofirm your e-mail{}.</h1>'.format(auth)
+        if(validate_email(email)):
+            con = mysql.get_db()
+            c = con.cursor()
+            c.execute('''Create TABLE IF NOT EXISTS acc(ID int Primary key auto_increment, email varchar(32), confirm Boolean)''')
+            c.execute('SELECT count(*) from acc where email = %s group by email',(email))
+            auth = c.rowcount
+            if(auth == 0):
+               c.execute('insert into acc(email, confirm) values(%s, false)', (email))
+               con.commit()
+               c.close()
+               msg = Message('Confirm Email', sender='test11aatest@gmail.com', recipients=[email])
+               ts = time.time()
+               link = url_for('confirm_email', ts=ts, email=email, _external=True)
+               msg.body = 'Your link is {}'.format(link)
+               mail.send(msg)
+               return '<h1>Please cofirm your e-mail{}.</h1>'.format(auth)
+            else:
+                return '<h1>e-mail exist: {}</h1>'.format(auth)
         else:
-            return '<h1>e-mail exist: {}</h1>'.format(auth)
+            return 'Invalid E-mail' 
 
            
 

8e1c64c43dfc1eab0b25a8cbf49663bdd4577171
ABortoc <abortoc@gmail.com>
2018-12-03 21:00:45 -0500

    Added fec data. Also there is a new csv file that has fec ids for the 2018 election cycle

--- functions.py
+++ functions.py
@@ -13,6 +13,9 @@ service = build('civicinfo', 'v2', developerKey=GOOGLE_API_FILE.readline())
 VOTESMART_API_FILE = open("votesmart_api.txt", "r")
 VOTESMART_API_KEY = VOTESMART_API_FILE.readline()
 
+FEC_API_FILE = open("fec_api.txt", "r")
+FEC_API_KEY = FEC_API_FILE.readline()
+
 app = Flask(__name__)
 
 #function to get candidates and offices they are running for based on an address
@@ -155,23 +158,70 @@ def getCandidateInfo(candidate_id):
 #Going to http://127.0.0.1:5000/getCandidatesInfo will return json about the selected candidate
 @app.route('/getCandidatesInfo', methods=['GET', 'POST'])
 def candidateSearch():
-    #address = "3039 Ocean Pkwy Brooklyn NY 11235"
-    #getCandidatesByOffice(address)
     if request.method == 'POST':
         print(request.form['name'], '\n', request.form['state'], '\n')
-        #candidate_name = request.args.get('name', None)
         candidate_name = request.form['name']
         name = candidate_name.split()
         first_name = name[0].title()
         last_name = name[1].title()
-        #candidate_state = request.args.get('state', None)
         candidate_state = request.form['state']
         candidate_state = stateConversion(candidate_state)
         id = getCandidateId(first_name, last_name, candidate_state)
-        print(id)
+        fec_id = getFecId(first_name, last_name, candidate_state)
+        print(fec_id)
         if id != 0:
-            candidates_info = getCandidateInfo(id)
+            candidates_info = getCandidateInfo(180416)
             return candidates_info
+
+
+def getFecId(first_name, last_name, state):
+    with open('candidate_summary_2018.csv', encoding="utf8") as csv_fec_file:
+       csv_reader_fec = csv.reader(csv_fec_file, delimiter=',')
+       line_count = 0
+       candidate_fec_id = 0
+       for row in csv_reader_fec:
+           if first_name and last_name in row[1]:
+               if state in row[4]:
+                   candidate_fec_id = row[2]  #can have multiples, need to think about it
+                   line_count += 1
+           line_count += 1
+       return candidate_fec_id
+
+def getFecInfo(candidate_fec_id):
+    with app.app_context():
+        response3 = requests.get("https://api.open.fec.gov/v1/candidate/{}/totals/?page=1&sort_null_only=false&sort_hide_null=false&sort=-cycle&sort_nulls_last=false&api_key={}&per_page=20".format(candidate_fec_id, FEC_API_KEY)).json()
+        fec_info = {}
+        for item in response3.get('results', []):
+            fec_info.update({"Total Contributions" : item.get('contributions'),
+                            "Total Individual Contributions" : item.get('individual_contributions'),
+                            "Itemized Individual Contributions" : item.get('individual_itemized_contributions'),
+                            "Unitemized Individual Contributions" : item.get('individual_unitemized_contributions'),
+                            "Party Committee Contributions" : item.get('political_party_committee_contributions'),
+                            "Political Committee Contributions" : item.get('other_political_committee_contributions'),
+                            "Transfers From Other Authorized Committees" : item.get('transfers_from_other_authorized_committee'),
+                            "Total Loans Received" : item.get('loans'),
+                            "Total Disbursements" : item.get('disbursements')
+                            })
+        resp3 = jsonify(fec_information=fec_info)
+        resp3.headers.add('Access-Control-Allow-Origin', '*')
+        return resp3
+
+@app.route('/getFecInfo', methods=['GET', 'POST'])
+def fecSearch():
+    if request.method == 'POST':
+        print(request.form['name'], '\n', request.form['state'], '\n')
+        candidate_name = request.form['name']
+        name = candidate_name.split()
+        first_name = name[0].upper()
+        last_name = name[1].upper()
+        candidate_state = request.form['state']
+        candidate_state = stateConversion(candidate_state)
+        fec_id = getFecId(first_name, last_name, candidate_state)
+        print(fec_id)
+        if fec_id != 0:
+            fec_candidate_info = getFecInfo(fec_id)
+            return fec_candidate_info
+
 
 if __name__ == '__main__':
     app.run(port = 5000, debug = True)
